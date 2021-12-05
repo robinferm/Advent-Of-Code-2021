c43d277d9ecc19f48f43b916be886731d432eead
Robin Ferm <robinferm@live.com>
2021-12-05 20:23:37 +0100

    Day 5 - part 2

--- 5/5.py
+++ 5/5.py
@@ -14,8 +14,8 @@ class Point:
 def read_file(file):
     return [list(x.replace(" -> ", "#").rstrip().split("#")) for x in open(os.path.join(sys.path[0],file)).readlines()]
 
-def create_points():
-    lines = read_file("input.txt")
+def create_points(file):
+    lines = read_file(file)
     points = []
     for line in lines:
         # p1
@@ -40,8 +40,6 @@ def print_matrix(matrix):
         print(x)
 
 def count_matrix(matrix):
-    # Count 2s in matrix
-    #return sum(x.count(2) for x in matrix)
     # Count greater than 1 in matrix
     arr = numpy.asarray(matrix)
     return (arr > 1).sum()
@@ -52,19 +50,15 @@ def count_matrix(matrix):
 
 
 
-def part1():
-    newlines = create_points()
-    matrix = create_matrix(len(newlines)*2)
+def part1(test = False):
+    newlines = create_points("testinput.txt") if test else create_points("input.txt")
+    matrix = create_matrix(10) if test else create_matrix(1000)
     for line in newlines:
-        print(line)
 
-        startX = line[0].X          # 0
-        startY = line[0].Y          # 9
-        #matrix[startX][startY] += 1
-
-        endX = line[1].X            # 5
-        endY = line[1].Y            # 9
-        #matrix[endX][endY] += 1
+        startX = line[0].X
+        startY = line[0].Y
+        endX = line[1].X
+        endY = line[1].Y
 
         # loop Y
         if startX == endX:
@@ -87,21 +81,16 @@ def part1():
 
 
 
-def part2():
-    newlines = create_points()
-    matrix = create_matrix(len(newlines)*2)
+
+def part2(test = False):
+    newlines = create_points("testinput.txt") if test else create_points("input.txt")
+    matrix = create_matrix(10) if test else create_matrix(1000)
     for line in newlines:
-        print(line)
-        # print(x[0].Y)
 
-
-        startX = line[0].X          # 0
-        startY = line[0].Y          # 9
-        #matrix[startX][startY] += 1
-
-        endX = line[1].X            # 5
-        endY = line[1].Y            # 9
-        #matrix[endX][endY] += 1
+        startX = line[0].X
+        startY = line[0].Y
+        endX = line[1].X
+        endY = line[1].Y
 
         # loop Y
         if startX == endX:
@@ -109,12 +98,7 @@ def part2():
             max_endY = max([startY, endY])
 
             for i in range(min_startY, max_endY+1):
-                #print(f"Append: {i},{startX}")
-                if matrix[i][startX] == 0:
-                    matrix[i][startX] = 1
-                elif matrix[i][startX] == 1:
-                    matrix[i][startX] = 2
-                # matrix[i][startX] += 1
+                matrix[i][startX] += 1
 
         # loop X
         if startY == endY:
@@ -122,115 +106,38 @@ def part2():
             max_endX = max([startX, endX])
 
             for i in range(min_startX, max_endX+1):
-                #print(f"Append: {startY},{i}")
-                if matrix[startY][i] == 0:
-                    matrix[startY][i] = 1
-                elif matrix[startY][i] == 1:
-                    matrix[startY][i] = 2
-                # matrix[startY][i] += 1
-
+                matrix[startY][i] += 1
 
         # Diagonal
-        # BÖrja alltid från vänster? 
-        if startX < endX and startY < endY: # \ top > down
-            print(f"Start: {startX} {startY}, end: {endX} {endY}")
+
+        # \
+        # Handle right to left
+        if startX > endX and startY > endY:
+            startX, endX = endX, startX
+            startY, endY = endY, startY
+        
+        if startX < endX and startY < endY: # 0,0 -> 8,8 \ top -> bot
             sx = startX
             sy = startY
-            while (sx != endX and sy != endY):
-                #matrix[sx][sy] = 5
-                if matrix[sx][sy] == 0:
-                    matrix[sx][sy] = 1
-                elif matrix[sx][sy] == 1:
-                    matrix[sx][sy] = 2
-                # matrix[startY][i] += 1
-                # x+1, y+1
+            while (sx != endX+1 and sy != endY+1):
+                matrix[sy][sx] += 1
                 sx += 1
                 sy += 1
+        # /
+        # Handle right to left
+        if startX > endX and startY < endY: # 
+            startX, endX = endX, startX
+            startY, endY = endY, startY
 
-        if startX > endX and startY < endY: # \ # bot > up
-            print(f"Start: {startX} {startY}, end: {endX} {endY}")
+        if startX < endX and startY > endY: # 0,8 -> 8,0 / bot -> top
             sx = startX
             sy = startY
-            while (sx != endX and sy != endY):
-                #matrix[sx][sy] = 5
-                if matrix[sx][sy] == 0:
-                    matrix[sx][sy] = 1
-                elif matrix[sx][sy] == 1:
-                    matrix[sx][sy] = 2
-                #matrix[sx][sy] = 5
-                # x+1, y-1
-                sy += 1
-                sx -= 1
-
-        if startX < endX and startY > endY: # / # bot > up
-            print(f"Start: {startX} {startY}, end: {endX} {endY}")
-            sx = startX
-            sy = startY
-            while (sx != endX and sy != endY):
-                #matrix[sx][sy] = 5
-                if matrix[sx][sy] == 0:
-                    matrix[sx][sy] = 1
-                elif matrix[sx][sy] == 1:
-                    matrix[sx][sy] = 2
-                #matrix[sx][sy] = 5
-                # x+1, y-1
-                sy -= 1
+            while (sx != endX+1 and sy != endY-1):
+                matrix[sy][sx] += 1
                 sx += 1
-
-        if startX > endX and startY > endY: # / # bot > up
-            print(f"Start: {startX} {startY}, end: {endX} {endY}")
-            sx = startX
-            sy = startY
-            while (sx != endX and sy != endY):
-                #matrix[sx][sy] = 5
-                if matrix[sx][sy] == 0:
-                    matrix[sx][sy] = 1
-                elif matrix[sx][sy] == 1:
-                    matrix[sx][sy] = 2
-                #matrix[sx][sy] = 5
-                # x+1, y-1
                 sy -= 1
-                sx -= 1
 
-    print_matrix(matrix)
     return count_matrix(matrix)
 
 print(part1())
-#print(part2())        
-        
-        # x+1, y-1
-    # if startY > endY: # / bot > up
-    #     # y+1
-    # if startY < endY: # \ top > down
-    #     # y-1
-
-    # min_startX = min([startX, endX])
-    # max_endX = max([startX, endX])
-    # for i in range(min_startX, max_endX+1):
-        
-    #     #print(f"Append: {startY},{i}")
-    #     if matrix[startY][i] == 0:
-    #         matrix[startY][i] = 1
-    #     elif matrix[startY][i] == 1:
-    #         matrix[startY][i] = 2
-    #     # matrix[startY][i] += 1
-
-    # min_startY = min([startY, endY])
-    # max_endY = max([startY, endY])
-
-    # for i in range(min_startY, max_endY+1):
-    #     #print(f"Append: {i},{startX}")
-    #     if matrix[i][startX] == 0:
-    #         matrix[i][startX] = 1
-    #     elif matrix[i][startX] == 1:
-    #         matrix[i][startX] = 2
-    #     # matrix[i][startX] += 1
-
-
-# 00 01 02
-# 10 11 12
-# 20 21 22
-
-# 00 11 22
-# x+1
-# y+1
+print(part2())
